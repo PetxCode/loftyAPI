3b4b0c0b084010360b9907ea58a9aca455915668
codelabite <chukwusahpeter@gmail.com>
2021-04-15 01:03:39 +0100

    adding online port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const port = 22001;
+const port = process.env.PORT || 22001;
 const app = express();
 const ONLINE_DB =
   "mongodb+srv://Achievers:Achievers@cluster0.xg4zd.mongodb.net/money?retryWrites=true&w=majority";
 const CodeLab = require("./MVC/Controller");
+
 mongoose
   .connect(ONLINE_DB, {
     useCreateIndex: true,
